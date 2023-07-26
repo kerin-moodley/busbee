94dc66e44648203ee24476436b46744a5b894b6b
kimmie-78 <101290243+kimmie-78@users.noreply.github.com>
2023-07-26 15:48:40 +0200

    Added in recapatcha backend code

--- busbee/login.aspx.cs
+++ busbee/login.aspx.cs
@@ -17,36 +17,76 @@ namespace busbee
 
         }
 
-        protected void btnSignIn_Click(object sender, EventArgs e)
-        {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["BusbeeConnection"].ConnectionString);
-            try
-            {
-                string User_Username = txtUsername.Text;
-                string User_Password = txtPassword.Text;
-                conn.Open();
-                string qry = "SELECT * FROM Commuter WHERE Commuter_Username='" + User_Username + "' and Commuter_Password='" + User_Password + "'";
-                SqlCommand cmd = new SqlCommand(qry, conn);
-                SqlDataReader sdr = cmd.ExecuteReader();
-                if (sdr.Read())
-                {
-                    string Message = "Login successful!";
-                    ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Message + "');", true);
-                    Response.Redirect("dashboard.aspx");
-                    
-                }
-                else
-                {
-                    string Message = "Login unsuccessful!";
-                    ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Message + "');", true);
+      protected void btnSignIn_Click(object sender, EventArgs e)
+{
+    // Retrieve user input
+    string User_Username = txtUsername.Text;
+    string User_Password = txtPassword.Text;
 
-                }
-                conn.Close();
-            }
-            catch (Exception ex)
-            {
-                Response.Write(ex.Message);
-            }
+    // Validate reCAPTCHA response
+    string reCaptchaResponse = Request.Form["g-recaptcha-response"];
+    bool isCaptchaValid = ValidateReCaptcha(reCaptchaResponse);
+
+    if (!isCaptchaValid)
+    {
+        string Message = "Please complete the reCAPTCHA verification!";
+        ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Message + "');", true);
+        return;
+    }
+
+    // Proceed with database authentication
+    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["BusbeeConnection"].ConnectionString);
+
+    try
+    {
+        conn.Open();
+        string qry = "SELECT * FROM Commuter WHERE Commuter_Username='" + User_Username + "' and Commuter_Password='" + User_Password + "'";
+        SqlCommand cmd = new SqlCommand(qry, conn);
+        SqlDataReader sdr = cmd.ExecuteReader();
+
+        if (sdr.Read())
+        {
+            string Message = "Login successful!";
+            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Message + "');", true);
+            Response.Redirect("dashboard.aspx");
         }
+        else
+        {
+            string Message = "Login unsuccessful!";
+            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + Message + "');", true);
+        }
+
+        conn.Close();
+    }
+    catch (Exception ex)
+    {
+        Response.Write(ex.Message);
+    }
+}
+
+// Function to validate reCAPTCHA response
+private bool ValidateReCaptcha(string reCaptchaResponse)
+{
+    string secretKey = "6Ld9AEInAAAAAIkdpQKiO7RMWtcVg3mBk55ja-DH"; 
+    string apiUrl = "https://www.google.com/recaptcha/api/siteverify";
+
+    using (var client = new System.Net.WebClient())
+    {
+        var response = client.DownloadString($"{apiUrl}?secret={secretKey}&response={reCaptchaResponse}");
+        var result = Newtonsoft.Json.JsonConvert.DeserializeObject<ReCaptchaResponse>(response);
+        return result.Success;
+    }
+}
+
+// Helper class for reCAPTCHA API response
+public class ReCaptchaResponse
+{
+    public bool Success { get; set; }
+    public string ChallengeTs { get; set; }
+    public string Hostname { get; set; }
+    public string ErrorCodes { get; set; }
+}
+
+
     }
 }
